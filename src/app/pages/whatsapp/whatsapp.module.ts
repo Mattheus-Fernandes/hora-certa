1d65fda5e19fc1e435de2e8fd3114caa0ea204f7
Mattheus Fernandes <mattheus.ale@hotmail.com>
2026-08-08 21:43:11 -0300

    feat: implementado pagina do whatsapp em desenvolvimento

--- src/app/pages/whatsapp/whatsapp.module.ts
+++ src/app/pages/whatsapp/whatsapp.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { WhatsappRoutingModule } from './whatsapp-routing.module';
+import { WhatsappComponent } from './whatsapp.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    WhatsappComponent
+  ],
   imports: [
     CommonModule,
-    WhatsappRoutingModule
+    WhatsappRoutingModule,
+    SharedModule
   ]
 })
 export class WhatsappModule { }
