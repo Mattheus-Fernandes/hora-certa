ef3bb2ef14241f3a8367ab0677d7501557836e3e
Mattheus Fernandes <mattheus.ale@hotmail.com>
2026-08-18 19:56:29 -0300

    feat: implementado metodo para buscar agendamentos finalizados

--- src/app/core/services/appointment.service.ts
+++ src/app/core/services/appointment.service.ts
@@ -86,4 +86,13 @@ export class AppointmentService {
     })
   }
 
+  getAllAppointmentsCompleteds(month: string, year: string): Observable<AppointmentsList> {
+    return this._http.get<AppointmentsList>(`${this._url}/completed`, {
+      params: {
+        month,
+        year
+      }
+    })
+  }
+
 }
