4b5226298405f69daccc845077925db313f0524e
Mattheus Fernandes <mattheus.ale@hotmail.com>
2026-08-18 19:56:02 -0300

    fix: correcao de identacao

--- src/app/pages/appointments/features/canceleds/canceleds.component.ts
+++ src/app/pages/appointments/features/canceleds/canceleds.component.ts
@@ -12,49 +12,49 @@ import { AppointmentsList } from 'src/app/core/types/appointments-list.type';
 export class CanceledsComponent implements OnInit {
 
   private readonly _appointmentsService = inject(AppointmentService)
-  
-    appointmentsList: AppointmentsList = []
-    appointmentsListFiltered: AppointmentsList = []
-    appointmentDate = new FormControl<Date | null>(new Date())
-    nzStatus: '' | 'error' = ''
-  
-    ngOnInit() {
-      this.getAllAppointmentsReminds()
+
+  appointmentsList: AppointmentsList = []
+  appointmentsListFiltered: AppointmentsList = []
+  appointmentDate = new FormControl<Date | null>(new Date())
+  nzStatus: '' | 'error' = ''
+
+  ngOnInit() {
+    this.getAllAppointmentsReminds()
+  }
+
+  private getAllAppointmentsReminds() {
+
+    const month = new Date().getMonth() + 1
+    const year = new Date().getFullYear()
+
+    this._appointmentsService.getAllAppointmentsCanceleds(month.toString(), year.toString())
+      .pipe(
+        take(1)
+      )
+      .subscribe((res: AppointmentsList) => {
+        this.appointmentsListFiltered = res
+      })
+  }
+
+  btnFilter() {
+    this.nzStatus = ""
+
+    if (!this.appointmentDate.value) {
+      this.nzStatus = "error"
+      return
     }
-  
-    private getAllAppointmentsReminds() {
-  
-      const month = new Date().getMonth() + 1
-      const year = new Date().getFullYear()
-  
-      this._appointmentsService.getAllAppointmentsCanceleds(month.toString(), year.toString())
-        .pipe(
-          take(1)
-        )
-        .subscribe((res: AppointmentsList) => {
-          this.appointmentsListFiltered = res
-        })
-    }
-  
-    btnFilter() {
-      this.nzStatus = ""
-  
-      if(!this.appointmentDate.value) {
-        this.nzStatus = "error"
-        return
-      }
-  
-      const month = Number(this.appointmentDate.value?.getMonth()) + 1
-      const correctMonth = month < 10 ? `0${month}` : month.toString()
-      const year = this.appointmentDate.value?.getFullYear().toString() as string
-  
-      this._appointmentsService.getAllAppointmentsCanceleds(correctMonth, year)
-        .pipe(
-          take(1)
-        )
-        .subscribe((res: AppointmentsList) => {
-          this.appointmentsListFiltered = res
-        })  
-    }
+
+    const month = Number(this.appointmentDate.value?.getMonth()) + 1
+    const correctMonth = month < 10 ? `0${month}` : month.toString()
+    const year = this.appointmentDate.value?.getFullYear().toString() as string
+
+    this._appointmentsService.getAllAppointmentsCanceleds(correctMonth, year)
+      .pipe(
+        take(1)
+      )
+      .subscribe((res: AppointmentsList) => {
+        this.appointmentsListFiltered = res
+      })
+  }
 
 }
