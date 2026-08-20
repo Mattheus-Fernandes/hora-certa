0b787681a7ba0c2f1ef8ff3d60c68da20879517b
Mattheus Fernandes <mattheus.ale@hotmail.com>
2026-08-20 09:55:00 -0300

    refactor: altera params metodo buscar agendamentos confirmados

--- src/app/core/services/appointment.service.ts
+++ src/app/core/services/appointment.service.ts
@@ -68,11 +68,10 @@ export class AppointmentService {
     return this._http.get<AppointmentsList>(`${this._url}/reminds`)
   }
 
-  getAllAppointmentsConfirmeds(month: string, year: string): Observable<AppointmentsList> {
+  getAllAppointmentsConfirmeds(date: string): Observable<AppointmentsList> {
     return this._http.get<AppointmentsList>(`${this._url}/confirmed`, {
       params: {
-        month,
-        year
+        date
       }
     })
   }
