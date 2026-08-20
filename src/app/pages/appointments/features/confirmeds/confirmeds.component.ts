4cff8c2db04493409ba4ac9cbcbc1142cfeea92f
Mattheus Fernandes <mattheus.ale@hotmail.com>
2026-08-20 09:55:08 -0300

    fix: correcao nome do metodo
    refactor: reutilizacao valor mes e ano
    
    refactor: limpeza de codigo

--- src/app/pages/appointments/features/confirmeds/confirmeds.component.ts
+++ src/app/pages/appointments/features/confirmeds/confirmeds.component.ts
@@ -3,6 +3,7 @@ import { FormControl } from '@angular/forms';
 import { take } from 'rxjs';
 import { AppointmentService } from 'src/app/core/services/appointment.service';
 import { AppointmentsList } from 'src/app/core/types/appointments-list.type';
+import { TransformDate } from 'src/app/core/utils/transform-date';
 
 @Component({
   selector: 'app-confirmeds',
@@ -19,15 +20,11 @@ export class ConfirmedsComponent implements OnInit {
   nzStatus: '' | 'error' = ''
 
   ngOnInit() {
-    this.getAllAppointmentsReminds()
+    this.getAllAppointmentsConfirmeds()
   }
 
-  private getAllAppointmentsReminds() {
-
-    const month = new Date().getMonth() + 1
-    const year = new Date().getFullYear()
-
-    this._appointmentsService.getAllAppointmentsConfirmeds(month.toString(), year.toString())
+  private getAllAppointmentsConfirmeds() {
+    this._appointmentsService.getAllAppointmentsConfirmeds(this.getMonthAndYear())
       .pipe(
         take(1)
       )
@@ -39,22 +36,24 @@ export class ConfirmedsComponent implements OnInit {
   btnFilter() {
     this.nzStatus = ""
 
-    if(!this.appointmentDate.value) {
+    if (!this.appointmentDate.value) {
       this.nzStatus = "error"
       return
     }
 
-    const month = Number(this.appointmentDate.value?.getMonth()) + 1
-    const correctMonth = month < 10 ? `0${month}` : month.toString()
-    const year = this.appointmentDate.value?.getFullYear().toString() as string
-
-    this._appointmentsService.getAllAppointmentsConfirmeds(correctMonth, year)
+    this._appointmentsService.getAllAppointmentsConfirmeds(this.getMonthAndYear())
       .pipe(
         take(1)
       )
       .subscribe((res: AppointmentsList) => {
         this.appointmentsListFiltered = res
       })
+  }
 
+  private getMonthAndYear(): string {
+    const month = Number(this.appointmentDate.value?.getMonth()) + 1
+    const year = this.appointmentDate.value?.getFullYear()
+
+    return TransformDate.apiFormat(String(month), String(year))
   }
 }
